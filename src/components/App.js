5c874286c86fbe94c99d0d9e887e522adafafb7f
lucykearl <104901670+lucykearl@users.noreply.github.com>
2022-09-17 13:52:39 +0100

    Forecast summary & forecast summaries

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
+import React from "react";
 import "../styles/App.css";
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
 
-function App() {
+function App({ forecasts, location }) {
   return (
-    <div className="App">
-        <h1>Weather App</h1>
+    <div className="forecast">
+      <LocationDetails city={location.city} country={location.country} />
+      <ForecastSummaries forecasts={forecasts} />
     </div>
   );
 }
+
+App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.string,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
 
 export default App;
